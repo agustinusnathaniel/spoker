8c9cd86ce60a92db5ffc3863bc2d0069a6e35f59
Agustinus Nathaniel <berserk.nate@gmail.com>
2026-03-20 23:14:18 +0700

    refactor: adjust next config to ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-const { withSentryConfig } = require('@sentry/nextjs');
+import { withSentryConfig } from '@sentry/nextjs';
+import type { NextConfig } from 'next';
 
-/** @type {import('next').NextConfig} */
-const moduleExports = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   redirects: async () => [
     {
@@ -15,9 +15,8 @@ const moduleExports = {
   },
 };
 
-module.exports = withSentryConfig(moduleExports, {
+export default withSentryConfig(nextConfig, {
   silent: true,
-  hideSourceMaps: true,
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 });
